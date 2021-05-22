1eaf17cd3534647174cf86955266c3b2b334f2d6
JungHyoMoon <dsinhome70@gmail.com>
2021-05-22 13:55:54 +0900

    working on details container. id check is done, todo: verify movie, show

--- src/routes/Details/DetailsContainer.js
+++ src/routes/Details/DetailsContainer.js
@@ -9,7 +9,21 @@ class DetailsContainer extends React.Component {
         loading: true,
     };
 
+    async componentDidMount() {
+        const {
+            match: {
+                params: { id },
+            },
+            history: { push },
+        } = this.props;
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)) {
+            push("/");
+        }
+    }
+
     render() {
+        console.log(this.props);
         const { result, error, loading } = this.state;
         return (
             <DetailsPresenter result={result} error={error} loading={loading} />
