75499c7c35f7a81dbcd0f7e3bf22a45b3445681e
JungHyoMoon <dsinhome70@gmail.com>
2021-06-05 16:09:04 +0900

    home, tv, search poster is done, todo: style for poster component

--- src/routes/TVshows/TVshowsPresenter.js
+++ src/routes/TVshows/TVshowsPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -18,21 +19,48 @@ const TVshowsPresenter = ({ topRated, popular, airingToday, loading, error }) =>
             {topRated && topRated.length > 0 && (
                 <Section title="Top Rated Shows">
                     {topRated.map((show) => (
-                        <span key={show.id}>{show.name}</span>
+                        <Poster
+                            id={show.id}
+                            imageUrl={show.poster_path}
+                            title={show.original_name}
+                            rating={show.vote_average}
+                            year={
+                                show.first_air_date &&
+                                show.first_air_date.substring(0, 4)
+                            }
+                        />
                     ))}
                 </Section>
             )}
             {popular && popular.length > 0 && (
                 <Section title="Popular Shows">
                     {popular.map((show) => (
-                        <span key={show.id}>{show.name}</span>
+                        <Poster
+                            id={show.id}
+                            imageUrl={show.poster_path}
+                            title={show.original_name}
+                            rating={show.vote_average}
+                            year={
+                                show.first_air_date &&
+                                show.first_air_date.substring(0, 4)
+                            }
+                        />
                     ))}
                 </Section>
             )}
             {airingToday && airingToday.length > 0 && (
                 <Section title="Shows Airing Today">
                     {airingToday.map((show) => (
-                        <span key={show.id}>{show.name}</span>
+                        <Poster
+                            id={show.id}
+                            imageUrl={show.poster_path}
+                            title={show.original_name}
+                            rating={show.vote_average}
+                            year={
+                                show.first_air_date &&
+                                show.first_air_date.substring(0, 4)
+                            }
+                        />
                     ))}
                 </Section>
             )}
