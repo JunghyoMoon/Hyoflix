4787c17bbf581a0537e10f55cf8ee7f88777756a
JungHyoMoon <dsinhome70@gmail.com>
2021-06-08 15:10:58 +0900

    test for detailed information about each movie, todo: make this shit beautiful and how to get trailer by api

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+// import PropTypes from "prop-types";
+
+const Container = styled.div``;
+
+const VideoPlayer = () => <Container></Container>;
+
+// VideoPlayer.propTypes = {};
+
+export default VideoPlayer;
--- src/routes/Details/DetailsPresenter.js
+++ src/routes/Details/DetailsPresenter.js
@@ -63,6 +63,8 @@ const DetailsPresenter = ({ result, error, loading }) =>
                             : require("../../assets/noPosterSmall.png")
                     }
                 />
+                <Title>{result.title}</Title>
+                <Data>{result.overview}</Data>
             </Content>
         </Container>
     );
